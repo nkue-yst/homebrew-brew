b18f5d5810816baa8aacfd58421606b2d97767fb
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-07-18 03:42:55 +0900

    Add settings for iftat

---
+++ Formula/iftat.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Iftat < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/nkue-yst/iftat/releases/download/v0.3.7/iftat-0.3.7_darwin_amd64.tar.gz"
+  sha256 "e5089abd91b92d6a3f1cdce516484b6582bbfbe5b83d192df2196ff1dfa09da8"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test iftat`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
