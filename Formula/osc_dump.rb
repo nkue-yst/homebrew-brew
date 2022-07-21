b5ccb59bc2aab74be2951487e6ef7045d1dc8f13
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-07-21 18:28:01 +0900

    Update osc_dump

--- Formula/osc_dump.rb
+++ Formula/osc_dump.rb
@@ -1,14 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class OscDump < Formula
   desc "OSCメッセージをダンプするプログラム"
-  homepage "https://github.com/nkue-yst/osc_dump"
-  url "https://github.com/nkue-yst/osc_dump/releases/download/v0.0.3/osc_dump-0.0.3_darwin_amd64.tar.gz"
-  sha256 "dbb54dc600bcfc5b30888fd6a59cfb0d505e0e0b2440f88210c91ab1483e0060"
-  license "MIT"
+  homepage "https://github.com/nkue-yst/osc_dump/"
+  url "https://github.com/nkue-yst/osc_dump/releases/download/v0.0.5/osc_dump-0.0.5_darwin_amd64.tar.gz"
+  sha256 "7c492d99b23e25f8f0d9d4714257d5b36e743228fa12a096c8ff65d46a503c4f"
+  license ""
 
-  def install
-    bin.install "osc_dump"
+    def install
+        bin.install "osc_dump"
   end
 end
